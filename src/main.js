120a7353ebb4ce3ebe226ac44b29264f5028c346
Zero <Zero@JvandeiMac.local>
2018-01-19 18:15:24 +0800

    wechat-share

--- src/main.js
+++ src/main.js
@@ -2,21 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import WechatShare from 'johnny-wechat-share'
+import ImageShare from "./assets/img/end_screen.png"
 let option={}
 option.api='http://h5.2smart.cn/wechat/js/'
+let url = "http://h5.2smart.cn/2018/kaleidoscope/"
+let img = url + 'img/end_screen.png'
 option.shareData={
   appmessage: {
-    title: "",//好友分享标题
-    desc: "",//好友分享描述
-    img: "",//好友分享图片
-    link: "" //好友分享链接
+    title: "万花筒",//好友分享标题
+    desc: "上传任意图片你可以看到不一样的效果",//好友分享描述
+    img_url: img,//好友分享图片
+    link: url //好友分享链接
   }, timeline: {
-    title: "",//朋友圈分享标题
-    img: "",//朋友圈分享图片
-    link: ""//朋友圈分享链接
+    title: "万花筒test",//朋友圈分享标题
+    img_url: img,//朋友圈分享图片
+    link: url//朋友圈分享链接
   }
 }
-Vue.prototype.WECHAT_SHARE= new WechatShare('http://h5.2smart.cn',option);
+let shareURL = document.location.href;
+window.WECHAT_SHARE = new WechatShare(encodeURIComponent(shareURL), option);
 new Vue({
   el: '#app',
   render: h => h(App)
